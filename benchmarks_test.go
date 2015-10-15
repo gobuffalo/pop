12ab9da4ffa5ac1b6ae37b59432b5d04013dfe0d
Mark Bates <mark@markbates.com>
2015-10-15 10:45:57 -0400

    added a few more benchmarks

--- benchmarks_test.go
+++ benchmarks_test.go
@@ -1,6 +1,8 @@
 package pop_test
 
 import (
+	"fmt"
+	"strconv"
 	"testing"
 
 	"github.com/markbates/going/nulls"
@@ -64,4 +66,50 @@ func Benchmark_Find_Raw(b *testing.B) {
 			tx.Store.Get(u, "select * from users where id = ?", u.ID)
 		}
 	})
+}
+
+func Benchmark_translateOne(b *testing.B) {
+	q := "select * from users where id = ? and name = ? and email = ? and a = ? and b = ? and c = ? and d = ? and e = ? and f = ?"
+	for n := 0; n < b.N; n++ {
+		translateOne(q)
+	}
+}
+
+func Benchmark_translateTwo(b *testing.B) {
+	q := "select * from users where id = ? and name = ? and email = ? and a = ? and b = ? and c = ? and d = ? and e = ? and f = ?"
+	for n := 0; n < b.N; n++ {
+		translateTwo(q)
+	}
+}
+
+func translateOne(sql string) string {
+	curr := 1
+	out := make([]byte, 0, len(sql))
+	for i := 0; i < len(sql); i++ {
+		if sql[i] == '?' {
+			str := "$" + strconv.Itoa(curr)
+			for _, char := range str {
+				out = append(out, byte(char))
+			}
+			curr += 1
+		} else {
+			out = append(out, sql[i])
+		}
+	}
+	return string(out)
+}
+
+func translateTwo(sql string) string {
+	curr := 1
+	csql := ""
+	for i := 0; i < len(sql); i++ {
+		x := sql[i]
+		if x == '?' {
+			csql = fmt.Sprintf("%s$%d", csql, curr)
+			curr++
+		} else {
+			csql += string(x)
+		}
+	}
+	return csql
 }
