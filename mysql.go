2951091ce58a7d4f1e23e6b7919e38d7527a03d3
Mark Bates <mark@markbates.com>
2016-08-15 12:17:20 -0400

    damn travis

--- mysql.go
+++ mysql.go
@@ -55,7 +55,8 @@ func (m *MySQL) SelectMany(store Store, models *Model, query Query) error {
 
 func (m *MySQL) CreateDB() error {
 	c := m.ConnectionDetails
-	cmd := exec.Command("mysql", "-u"+c.User, "-p"+c.Password, "-e", fmt.Sprintf("create database %s", c.Database))
+	cmd := exec.Command("mysql", "-u "+c.User, "--password"+c.Password, "-e", fmt.Sprintf("create database %s", c.Database))
+	fmt.Printf("### cmd -> %#v\n", cmd)
 	return clam.RunAndListen(cmd, func(s string) {
 		fmt.Println(s)
 	})
