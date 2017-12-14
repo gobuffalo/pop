423efb1d6105623bc2c8c6feda317599edc18073
Matthew Clark <mclark4386@gmail.com>
2017-12-13 22:28:41 -0600

    *forgot to add indexinfo test and refactored schema building for tests

--- fizz/translators/schema_test.go
+++ fizz/translators/schema_test.go
@@ -5,13 +5,18 @@ import (
 	"github.com/markbates/pop/fizz/translators"
 )
 
-func (s *SchemaSuite) Test_Schema_TableInfo() {
-	r := s.Require()
+func (s *SchemaSuite) buildSchema() translators.Schema {
 	schema := map[string]*fizz.Table{}
 	ta := &fizz.Table{Name: "testTable"}
 	ta.Column("testColumn", "type", nil)
+	ta.Indexes = append(ta.Indexes, fizz.Index{Name: "testIndex"})
 	schema["testTable"] = ta
-	ts := translators.CreateSchema("name", "url", schema)
+	return translators.CreateSchema("name", "url", schema)
+}
+
+func (s *SchemaSuite) Test_Schema_TableInfo() {
+	r := s.Require()
+	ts := s.buildSchema()
 	t, err := ts.TableInfo("testTable")
 	r.NoError(err)
 	r.Equal("testTable", t.Name)
@@ -19,12 +24,16 @@ func (s *SchemaSuite) Test_Schema_TableInfo() {
 
 func (s *SchemaSuite) Test_Schema_ColumnInfo() {
 	r := s.Require()
-	schema := map[string]*fizz.Table{}
-	ta := &fizz.Table{Name: "testTable"}
-	ta.Column("testColumn", "type", nil)
-	schema["testTable"] = ta
-	ts := translators.CreateSchema("name", "url", schema)
+	ts := s.buildSchema()
 	c, err := ts.ColumnInfo("testTable", "testCOLUMN")
 	r.NoError(err)
 	r.Equal("testColumn", c.Name)
+}
+
+func (s *SchemaSuite) Test_Schema_IndexInfo() {
+	r := s.Require()
+	ts := s.buildSchema()
+	c, err := ts.IndexInfo("testTable", "testindEX")
+	r.NoError(err)
+	r.Equal("testIndex", c.Name)
 }
