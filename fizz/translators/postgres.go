5188f82e577b15101de2e02e0ae6c37146c04c21
Bogdan I. Bursuc <bogdanbursuc86@gmail.com>
2017-12-19 12:40:42 +0200

    Postgres Fizz FK support

--- fizz/translators/postgres.go
+++ fizz/translators/postgres.go
@@ -35,6 +35,11 @@ func (p *Postgres) CreateTable(t fizz.Table) (string, error) {
 		}
 		cols = append(cols, s)
 	}
+
+	for _, fk := range t.ForeignKeys {
+		cols = append(cols, p.buildForeignKey(t, fk, true))
+	}
+
 	s = fmt.Sprintf("CREATE TABLE \"%s\" (\n%s\n);", t.Name, strings.Join(cols, ",\n"))
 	sql = append(sql, s)
 
@@ -130,11 +135,27 @@ func (p *Postgres) RenameIndex(t fizz.Table) (string, error) {
 }
 
 func (p *Postgres) AddForeignKey(t fizz.Table) (string, error) {
-	return "", nil
+	if len(t.ForeignKeys) == 0 {
+		return "", errors.New("Not enough foreign keys supplied!")
+	}
+
+	return p.buildForeignKey(t, t.ForeignKeys[0], false), nil
 }
 
 func (p *Postgres) DropForeignKey(t fizz.Table) (string, error) {
-	return "", nil
+	if len(t.ForeignKeys) == 0 {
+		return "", errors.New("Not enough foreign keys supplied!")
+	}
+
+	fk := t.ForeignKeys[0]
+
+	var ifExists string
+	if v, ok := fk.Options["if_exists"]; ok && v.(bool) {
+		ifExists = "IF EXISTS"
+	}
+
+	s := fmt.Sprintf("ALTER TABLE %s DROP CONSTRAINT %s %s", t.Name, ifExists, fk.Name)
+	return s, nil
 }
 
 func (p *Postgres) buildAddColumn(c fizz.Column) string {
@@ -190,4 +211,23 @@ func (p *Postgres) colType(c fizz.Column) string {
 	default:
 		return c.ColType
 	}
+}
+
+func (p *Postgres) buildForeignKey(t fizz.Table, fk fizz.ForeignKey, onCreate bool) string {
+	refs := fmt.Sprintf("%s (%s)", fk.References.Table, strings.Join(fk.References.Columns, ", "))
+	s := fmt.Sprintf("FOREIGN KEY (%s) REFERENCES %s", fk.Column, refs)
+
+	if onUpdate, ok := fk.Options["on_update"]; ok {
+		s += fmt.Sprintf(" ON DELETE %s", onUpdate)
+	}
+
+	if onDelete, ok := fk.Options["on_delete"]; ok {
+		s += fmt.Sprintf(" ON DELETE %s", onDelete)
+	}
+
+	if !onCreate {
+		s = fmt.Sprintf("ALTER TABLE %s ADD CONSTRAINT %s %s", t.Name, fk.Name, s)
+	}
+
+	return s
 }
