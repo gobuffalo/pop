69ab5b8c30d238467dd1de55ffaa0b9168af5172
Mark Bates <mark@markbates.com>
2018-02-28 14:28:14 -0500

    removed unneeded Println

--- sqlite_shim.go
+++ sqlite_shim.go
@@ -4,12 +4,7 @@ package pop
 
 import (
 	"errors"
-	"fmt"
 )
-
-func init() {
-	fmt.Println("in shim")
-}
 
 func newSQLite(deets *ConnectionDetails) (dialect, error) {
 	return nil, errors.New("sqlite3 was not compiled into the binary")
