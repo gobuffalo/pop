b23b4da0e71ac6e172c62c3e1fcb28b0ec39bf84
Bart Willems <bwillems@protonmail.com>
2018-04-18 19:44:18 +0200

    Capitalize DESC in Last()
    Signed-off-by: Bart Willems <bwillems@protonmail.com>

--- finders.go
+++ finders.go
@@ -88,7 +88,7 @@ func (c *Connection) Last(model interface{}) error {
 func (q *Query) Last(model interface{}) error {
 	err := q.Connection.timeFunc("Last", func() error {
 		q.Limit(1)
-		q.Order("created_at desc")
+		q.Order("created_at DESC")
 		m := &Model{Value: model}
 		if err := q.Connection.Dialect.SelectOne(q.Connection.Store, m, *q); err != nil {
 			return err
