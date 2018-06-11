0f12d4038138a0f213024370ee23ff25c70a549f
Stanislas Michalak <stanislas.michalak@gmail.com>
2018-06-11 14:18:52 +0200

    Deprecate `migrate reset`, fallback to migrations when the schema can't be found

--- soda/cmd/reset.go
+++ soda/cmd/reset.go
@@ -4,6 +4,7 @@ import (
 	"os"
 
 	"github.com/gobuffalo/pop"
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 )
 
@@ -16,34 +17,29 @@ var resetCmd = &cobra.Command{
 	Use:   "reset",
 	Short: "Drop, then recreate databases",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		f, err := os.Open(resetOptions.input)
-		if err != nil {
-			return err
-		}
-		defer f.Close()
+		var schema *os.File
+		useMigrations := false
 
-		doReset := func(c *pop.Connection, f *os.File) error {
-			if err := pop.DropDB(c); err != nil {
+		if _, err := os.Stat(resetOptions.input); err == nil {
+			schema, err = os.Open(resetOptions.input)
+			if err != nil {
 				return err
 			}
-			if err := pop.CreateDB(c); err != nil {
-				return err
-			}
-			if err := c.Dialect.LoadSchema(f); err != nil {
-				return err
-			}
-			return nil
+			defer schema.Close()
+		} else {
+			// Fallback to migrations
+			useMigrations = true
 		}
 
 		if all {
 			for _, conn := range pop.Connections {
-				if err := doReset(conn, f); err != nil {
+				if err := doReset(conn, schema, useMigrations); err != nil {
 					return err
 				}
 			}
 		} else {
 			c := getConn()
-			if err := doReset(c, f); err != nil {
+			if err := doReset(c, schema, useMigrations); err != nil {
 				return err
 			}
 		}
@@ -56,4 +52,25 @@ func init() {
 	resetCmd.Flags().BoolVarP(&resetOptions.all, "all", "a", false, "Drops and recreate all of the databases in the database.yml")
 	resetCmd.Flags().StringVarP(&resetOptions.input, "input", "i", "schema.sql", "The path to the schema file you want to load")
 	RootCmd.AddCommand(resetCmd)
+}
+
+func doReset(c *pop.Connection, f *os.File, useMigrations bool) error {
+	if err := pop.DropDB(c); err != nil {
+		return err
+	}
+	if err := pop.CreateDB(c); err != nil {
+		return err
+	}
+	if useMigrations {
+		mig, err := pop.NewFileMigrator(migrationPath, getConn())
+		if err != nil {
+			return errors.WithStack(err)
+		}
+		return mig.Up()
+	}
+	// Use schema instead
+	if err := c.Dialect.LoadSchema(f); err != nil {
+		return err
+	}
+	return nil
 }
