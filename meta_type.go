43db518b73b845e6192791d97e5520092a0febd2
Larry M Jordan <larrymoralesjordan@gmail.com>
2018-06-12 10:07:32 -0500

    add support for slice and maps in metatype

--- meta_type.go
+++ meta_type.go
@@ -1,8 +1,11 @@
 package pop
 
 import (
+	"fmt"
 	"reflect"
 	"strings"
+
+	"github.com/gobuffalo/pop/nulls"
 )
 
 var associationTags = "belongs_to has_one has_many many_to_many"
@@ -15,7 +18,44 @@ type MetaType struct {
 	IndirectValue reflect.Value
 	IndirectType  reflect.Type
 	Fields        []*MetaType
+	Ptr           bool
 	tags          reflect.StructTag
+}
+
+type AssociationMetaType struct {
+	*MetaType
+	Root *MetaType
+}
+
+// Constraint returns the sql where clause to restrict query.
+// for this association.
+func (amt *AssociationMetaType) Constraint() string {
+	column := (&Model{Value: amt.Root.Value.Interface()}).associationName()
+
+	if amt.tags.Get("belongs_to") != "" {
+		column = "id"
+	}
+
+	if amt.tags.Get("fk_id") != "" {
+		column = amt.tags.Get("fk_id")
+	}
+
+	return fmt.Sprintf("%s in (?)", column)
+}
+
+// DependencyField returns the field that relates this association
+// with another.
+func (amt *AssociationMetaType) DependencyField() string {
+	if amt.Root.IndirectType.Kind() == reflect.Slice {
+		return fmt.Sprintf("%s%s", amt.Root.IndirectType.Elem().Name(), "ID")
+	}
+
+	return fmt.Sprintf("%s%s", amt.Root.IndirectType.Name(), "ID")
+}
+
+// Interface analogous to Interface method reflect.Value.
+func (t *MetaType) Interface() interface{} {
+	return t.Value.Interface()
 }
 
 func (t *MetaType) ffields(fn func(*MetaType) bool) []*MetaType {
@@ -28,10 +68,39 @@ func (t *MetaType) ffields(fn func(*MetaType) bool) []*MetaType {
 	return ffields
 }
 
+// FieldByName returns field specified with name. If metatype is
+// for an array and all elements are struct it will return all
+// field values for all elements.
+func (t *MetaType) FieldByName(field string) ReflectValues {
+	allValues := ReflectValues{}
+	if t.IndirectType.Kind() == reflect.Slice {
+		for i := 0; i < t.IndirectValue.Len(); i++ {
+			s := t.IndirectValue.Index(i)
+			if s.Kind() == reflect.Struct {
+				v := s.FieldByName(field)
+				allValues = append(allValues, v)
+			}
+		}
+		return allValues
+	}
+
+	if t.IndirectType.Kind() == reflect.Struct {
+		v := t.IndirectValue.FieldByName(field)
+		allValues = append(allValues, v)
+	}
+
+	return allValues
+}
+
 // Associations return metadata for associations defined in model t.
-func (t *MetaType) Associations() []*MetaType {
+func (t *MetaType) Associations() []*AssociationMetaType {
+	if t.IndirectType.Kind() == reflect.Slice {
+		v := innerValueForSlice(t)
+		return buildMetaType(v.Interface(), "").Associations()
+	}
+
 	atag := strings.Fields(associationTags)
-	return t.ffields(func(tm *MetaType) bool {
+	fields := t.ffields(func(tm *MetaType) bool {
 		for _, tag := range atag {
 			if tm.tags.Get(tag) != "" {
 				return true
@@ -39,6 +108,30 @@ func (t *MetaType) Associations() []*MetaType {
 		}
 		return false
 	})
+
+	assos := []*AssociationMetaType{}
+	for _, f := range fields {
+		assos = append(assos, &AssociationMetaType{f, t})
+	}
+	return assos
+}
+
+// Association returns only association with the specified tag name.
+func (t *MetaType) Association(kind string) []*AssociationMetaType {
+	if t.IndirectType.Kind() == reflect.Slice {
+		v := innerValueForSlice(t)
+		return buildMetaType(v.Interface(), "").Association(kind)
+	}
+
+	fields := t.ffields(func(tm *MetaType) bool {
+		return tm.tags.Get(kind) != ""
+	})
+
+	assos := []*AssociationMetaType{}
+	for _, f := range fields {
+		assos = append(assos, &AssociationMetaType{f, t})
+	}
+	return assos
 }
 
 // MakeSlice makes a new slice based on current MetaType type.
@@ -46,27 +139,146 @@ func (t *MetaType) MakeSlice() *MetaType {
 	switch t.Type.Kind() {
 	case reflect.Slice, reflect.Array:
 		sliceVal := reflect.MakeSlice(t.Type, t.Value.Len(), t.Value.Cap())
-		return buildMetaType(sliceVal.Interface(), "")
+		return buildMetaType(reflect.New(sliceVal.Type()).Interface(), "")
 	default:
 		sliceVal := reflect.MakeSlice(reflect.SliceOf(t.Type), 0, 0)
-		return buildMetaType(sliceVal.Interface(), "")
+		return buildMetaType(reflect.New(sliceVal.Type()).Interface(), "")
 	}
 }
 
 // MakeMap creates a map for a struct model type.
 func (t *MetaType) MakeMap() *MetaType {
+	switch t.IndirectType.Kind() {
+	case reflect.Struct:
+		return t.makeMapForStruct()
+	case reflect.Slice, reflect.Array:
+		return t.makeMapForSlice()
+	default:
+		mapMade := buildMap(reflect.TypeOf(""), t.Type)
+		return buildMetaType(mapMade.Interface(), "")
+	}
+}
+
+func (t *MetaType) makeMapForStruct() *MetaType {
 	fIDs := t.ffields(func(f *MetaType) bool {
 		return f.Name == "ID"
 	})
 
-	switch t.IndirectType.Kind() {
-	case reflect.Struct:
-		mapType := reflect.MapOf(fIDs[0].Type, t.IndirectType)
-		mapVal := reflect.MakeMap(mapType)
-		return buildMetaType(mapVal.Interface(), "")
-	default:
-		return nil
+	typ := t.IndirectType
+	if t.Ptr {
+		typ = reflect.PtrTo(t.IndirectType)
 	}
+	mapMade := buildMap(fIDs[0].Type, typ)
+
+	return buildMetaType(mapMade.Interface(), "")
+}
+
+func (t *MetaType) makeMapForSlice() *MetaType {
+	if t.IndirectType.Kind() != reflect.Slice {
+		panic(fmt.Sprintf("%s is not an array type", t.Type))
+	}
+
+	v := innerValueForSlice(t)
+	m := buildMetaType(v.Interface(), "").MakeMap()
+
+	if reflect.Indirect(v).Kind() == reflect.Struct {
+		if t.IndirectValue.Len() > 0 {
+			for i := 0; i < t.IndirectValue.Len(); i++ {
+				v := t.IndirectValue.Index(i)
+				m.AppendMap(reflect.Indirect(v).FieldByName("ID").Interface(), v.Addr().Interface())
+			}
+		}
+		return m
+	}
+
+	if t.IndirectValue.Len() > 0 {
+		for i := 0; i < t.IndirectValue.Len(); i++ {
+			v := t.IndirectValue.Index(i).Interface()
+			m.AppendMap(fmt.Sprintf("%s", v), v)
+		}
+	}
+	return m
+}
+
+// MakeMapWithField create a map for the specified MetaType with the key
+// indicated by the field passed as parameter.
+func (t *MetaType) MakeMapWithField(field string) *MetaType {
+	if t.IndirectType.Kind() == reflect.Slice || t.IndirectType.Kind() == reflect.Array {
+		val := innerValueForSlice(t)
+		fval := reflect.Indirect(val).FieldByName(field)
+
+		var m reflect.Value
+		if n := nulls.New(fval.Interface()); n != nil {
+			m = buildMap(reflect.TypeOf(n.Interface()), val.Type())
+		} else {
+			m = buildMap(fval.Type(), val.Type())
+		}
+
+		for i := 0; i < t.IndirectValue.Len(); i++ {
+			v := t.IndirectValue.Index(0)
+			f := v.FieldByName(field)
+			if n := nulls.New(f.Interface()); n != nil {
+				m.SetMapIndex(reflect.ValueOf(n.Interface()), v.Addr())
+			} else {
+				m.SetMapIndex(f, v.Addr())
+			}
+		}
+		return buildMetaType(m.Interface(), "")
+	}
+
+	if t.IndirectType.Kind() == reflect.Struct {
+		f := t.IndirectValue.FieldByName(field)
+		m := buildMap(f.Type(), t.Type)
+		m.SetMapIndex(f, t.Value)
+		return buildMetaType(m, "")
+	}
+	return nil
+}
+
+func innerValueForSlice(t *MetaType) reflect.Value {
+	var elemType reflect.Type
+	var v reflect.Value
+
+	// validates which slice inner element is.
+	if t.Ptr {
+		elemType = t.IndirectType.Elem()
+	} else {
+		elemType = t.Type.Elem()
+	}
+
+	// creates a new object type.
+	if elemType.Kind() == reflect.Ptr {
+		elemType = elemType.Elem()
+		v = reflect.New(elemType)
+	} else {
+		v = reflect.New(elemType)
+	}
+
+	return v
+}
+
+// AppendMap appends to this map a value specified by key.
+func (t *MetaType) AppendMap(key, value interface{}) {
+	if t.Type.Kind() != reflect.Map {
+		panic(fmt.Sprintf("%s is not a map", t.Type))
+	}
+	t.Value.SetMapIndex(reflect.ValueOf(key), reflect.ValueOf(value))
+}
+
+// MapKeys returns all keys registered in this map.
+func (t *MetaType) MapKeys() ReflectValues {
+	if t.Type.Kind() != reflect.Map {
+		panic(fmt.Sprintf("%s is not a map", t.Type))
+	}
+	return ReflectValues(t.Value.MapKeys())
+}
+
+// MapValue returns a value for the specified key.
+func (t *MetaType) MapValue(key interface{}) reflect.Value {
+	if t.Type.Kind() != reflect.Map {
+		panic(fmt.Sprintf("%s is not a map", t.Type))
+	}
+	return reflect.Indirect(t.Value.MapIndex(reflect.ValueOf(key)))
 }
 
 // Meta a helper function that wraps reflection data type
@@ -90,6 +302,8 @@ func buildMetaType(model interface{}, name string) *MetaType {
 		mm.Name = mm.IndirectType.Name()
 	}
 
+	mm.Ptr = mm.Type.Kind() == reflect.Ptr
+
 	if iv.Kind() == reflect.Struct {
 		mm.Fields = []*MetaType{}
 		for i := 0; i < iv.NumField(); i++ {
@@ -104,4 +318,24 @@ func buildMetaType(model interface{}, name string) *MetaType {
 	}
 
 	return mm
+}
+
+func buildMap(key, val reflect.Type) reflect.Value {
+	mapType := reflect.MapOf(key, val)
+	mapVal := reflect.MakeMap(mapType)
+	return mapVal
+}
+
+// ReflectValue is a helper to wrap some util functions for
+// reflect.Value slice.
+type ReflectValues []reflect.Value
+
+// Interface parses all values to interface. It is an analougus
+// to Interface method for reflect.Value.
+func (rv ReflectValues) Interface() []interface{} {
+	values := []interface{}{}
+	for _, v := range rv {
+		values = append(values, v.Interface())
+	}
+	return values
 }
--- meta_type_test.go
+++ meta_type_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/gobuffalo/pop"
+	"github.com/gobuffalo/pop/nulls"
 	"github.com/stretchr/testify/require"
 )
 
@@ -25,20 +26,51 @@ func Test_Model_Meta_Slice(t *testing.T) {
 	mm := m.Meta()
 	sl := mm.MakeSlice()
 
-	r.Equal(sl.Type.Kind(), reflect.Slice)
-	r.Equal(sl.Value.Len(), 0)
+	r.Equal(sl.IndirectType.Kind(), reflect.Slice)
+	r.Equal(sl.IndirectValue.Len(), 0)
 }
 
-func Test_Model_Meta_Map(t *testing.T) {
+func Test_Model_Meta_Map_For_Struct(t *testing.T) {
 	r := require.New(t)
 
-	u := &User{}
-	m := pop.Model{Value: u}
+	u := User{}
+	m := pop.Model{Value: &u}
 	mm := m.Meta()
 	sl := mm.MakeMap()
 
 	r.Equal(sl.Type.Kind(), reflect.Map)
-	r.Equal(sl.Value.Type(), reflect.MapOf(reflect.TypeOf(u.ID), mm.IndirectType))
+	r.Equal(sl.Value.Type(), reflect.MapOf(reflect.TypeOf(u.ID), mm.Type))
+}
+
+func Test_Model_Meta_Map_For_Slice(t *testing.T) {
+	r := require.New(t)
+
+	u := []*User{
+		&User{Email: "user@email.com"},
+	}
+	m := pop.Model{Value: &u}
+	mm := m.Meta()
+	sl := mm.MakeMap()
+
+	r.Equal(sl.Type, reflect.MapOf(reflect.TypeOf(0), reflect.TypeOf(&User{})))
+	r.Equal(1, len(sl.Value.Interface().(map[int]*User)))
+
+	// Map for non-struct with pointer.
+	n := 1
+	v := []*int{&n}
+	m = pop.Model{Value: &v}
+	mm = m.Meta()
+	sl = mm.MakeMap()
+	r.Equal(sl.Type, reflect.MapOf(reflect.TypeOf(""), reflect.TypeOf(v[0])))
+	r.Equal(1, len(sl.Value.Interface().(map[string]*int)))
+
+	// Map for non-struct without pointer.
+	v2 := []int{1}
+	m = pop.Model{Value: &v2}
+	mm = m.Meta()
+	sl = mm.MakeMap()
+	r.Equal(sl.Type, reflect.MapOf(reflect.TypeOf(""), reflect.TypeOf(v2[0])))
+	r.Equal(1, len(sl.Value.Interface().(map[string]int)))
 }
 
 func Test_Model_Meta_Associations(t *testing.T) {
@@ -49,4 +81,148 @@ func Test_Model_Meta_Associations(t *testing.T) {
 
 	mAssociations := mm.Associations()
 	r.Equal(3, len(mAssociations))
+}
+
+func Test_Model_Meta_Associations_Direct(t *testing.T) {
+	transaction(func(tx *pop.Connection) {
+		a := require.New(t)
+
+		for _, name := range []string{"Mark", "Joe", "Jane"} {
+			user := User{Name: nulls.NewString(name)}
+			err := tx.Create(&user)
+			a.NoError(err)
+
+			book := Book{UserID: nulls.NewInt(user.ID)}
+			err = tx.Create(&book)
+			a.NoError(err)
+
+			if name == "Mark" {
+				song := Song{UserID: user.ID}
+				err = tx.Create(&song)
+				a.NoError(err)
+
+				address := Address{Street: "Pop"}
+				err = tx.Create(&address)
+
+				home := UsersAddress{UserID: user.ID, AddressID: address.ID}
+				err = tx.Create(&home)
+			}
+		}
+
+		users := Users{}
+		tx.All(&users)
+
+		err := loadDirect(&users, tx, "has_many")
+		a.NoError(err)
+
+		err = loadDirect(&users, tx, "has_one")
+		a.NoError(err)
+
+		a.Equal(1, len(users[0].Books))
+		a.Equal(users[0].ID, users[0].FavoriteSong.UserID)
+		a.Zero(users[1].FavoriteSong.UserID)
+
+		books := Books{}
+		err = tx.All(&books)
+		a.NoError(err)
+
+		loadIndirect(&books, tx, "belongs_to")
+	})
+}
+
+func loadDirect(model interface{}, tx *pop.Connection, tag string) error {
+	// 1- transform into a model and get meta.
+	m := pop.Model{Value: model}
+	mm := m.Meta()
+	mmap := mm.MakeMap()
+
+	// 2- get all associations with tag specified.
+	assos := mm.Association(tag)
+
+	// 3- iterate and fill every has many association.
+	for _, asso := range assos {
+		assoSlice := asso.MakeSlice()
+		assoSliceInt := assoSlice.Interface()
+		args := mmap.MapKeys().Interface()
+
+		err := tx.Where(asso.Constraint(), args...).All(assoSliceInt)
+		if err != nil {
+			return err
+		}
+
+		// iterate over every slice element fill in the database.
+		assoSliceVal := assoSlice.IndirectValue
+		for i := 0; i < assoSliceVal.Len(); i++ {
+			elemVal := assoSliceVal.Index(i)
+
+			// Get the relationship field.
+			v := elemVal.FieldByName(asso.DependencyField())
+
+			// get the map value with the id specified.
+			var u reflect.Value
+			if n := nulls.New(v.Interface()); n != nil { // is a nulls type.
+				u = mmap.MapValue(n.Interface())
+			} else {
+				u = mmap.MapValue(v.Interface())
+			}
+
+			// get the association field of the map value and append value.
+			b := u.FieldByName(asso.Name)
+			if b.Kind() == reflect.Slice || b.Kind() == reflect.Array {
+				b.Set(reflect.Append(b, elemVal))
+			} else {
+				b.Set(elemVal)
+			}
+		}
+	}
+	return nil
+}
+
+func loadIndirect(model interface{}, tx *pop.Connection, tag string) error {
+	// 1- transform into a model and get meta.
+	m := pop.Model{Value: model}
+	mm := m.Meta()
+
+	// 2- get all associations with tag specified.
+	assos := mm.Association(tag)
+	args := mm.FieldByName("UserID").Interface()
+	mmap := mm.MakeMapWithField("UserID")
+
+	// 3- iterate and fill every has many association.
+	for _, asso := range assos {
+		assoSlice := asso.MakeSlice()
+		assoSliceInt := assoSlice.Interface()
+
+		err := tx.Where(asso.Constraint(), args...).All(assoSliceInt)
+		if err != nil {
+			return err
+		}
+
+		// iterate over every slice element fill in the database.
+		assoSliceVal := assoSlice.IndirectValue
+		for i := 0; i < assoSliceVal.Len(); i++ {
+			elemVal := assoSliceVal.Index(i)
+
+			// Get the relationship field.
+			v := elemVal.FieldByName("ID")
+			mmap.MapValue(v.Interface())
+
+			// get the map value with the id specified.
+			// var u reflect.Value
+			// if n := nulls.New(v.Interface()); n != nil { // is a nulls type.
+			// 	u = mmap.MapValue(n.Interface())
+			// } else {
+			// 	u = mmap.MapValue(v.Interface())
+			// }
+
+			// // get the association field of the map value and append value.
+			// b := u.FieldByName(asso.Name)
+			// if b.Kind() == reflect.Slice || b.Kind() == reflect.Array {
+			// 	b.Set(reflect.Append(b, elemVal))
+			// } else {
+			// 	b.Set(elemVal)
+			// }
+		}
+	}
+	return nil
 }
