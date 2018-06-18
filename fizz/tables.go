729382a128c4cbc226880ff4c94ab989c4b39f3d
Stanislas Michalak <stanislas.michalak@gmail.com>
2018-06-18 14:34:06 +0200

    Merge master into development

--- fizz/tables.go
+++ fizz/tables.go
@@ -20,7 +20,7 @@ func (t *Table) DisableTimestamps() {
 }
 
 // Column adds a new column to the table definition.
-func (t *Table) Column(name string, colType string, options map[string]interface{}) {
+func (t *Table) Column(name string, colType string, options Options) {
 	var primary bool
 	if _, ok := options["primary"]; ok {
 		primary = true
