70d11a955f51a98e66e9f034773aadac6dbef7ec
Unknown <baggerone@gmail.com>
2018-08-31 15:03:27 -0400

    Don't allow args and get user confirmation through a flag or input.

--- soda/cmd/drop.go
+++ soda/cmd/drop.go
@@ -5,15 +5,35 @@ import (
 
 	"github.com/gobuffalo/pop"
 	"github.com/spf13/cobra"
-)
+	"os"
+	"bufio"
+	)
 
 var all bool
+var confirmed bool
 
 var dropCmd = &cobra.Command{
 	Use:   "drop",
 	Short: "Drops databases for you",
 	Run: func(cmd *cobra.Command, args []string) {
 		var err error
+		if len(args) > 1 {
+			err = fmt.Errorf("No arguments are allowed with the drop database command.")
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		if !confirmed {
+			reader := bufio.NewReader(os.Stdin)
+			fmt.Print("Do you really want to drop the database (y)? ")
+			text, _ := reader.ReadString('\n')
+			if text != "y" && text != "Y" {
+				fmt.Println("Aborting due to lack of user confirmation.")
+				os.Exit(0)
+			}
+
+		}
+
 		if all {
 			for _, conn := range pop.Connections {
 				err = pop.DropDB(conn)
@@ -31,5 +51,6 @@ var dropCmd = &cobra.Command{
 
 func init() {
 	dropCmd.Flags().BoolVarP(&all, "all", "a", false, "Drops all of the databases in the database.yml")
+	dropCmd.Flags().BoolVarP(&confirmed, "confirmed", "y", false, "Runs without asking the user for confirmation")
 	RootCmd.AddCommand(dropCmd)
 }
