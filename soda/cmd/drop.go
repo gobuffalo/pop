c1ec6259eb8cbcc91b2f394cf983009ab2573aae
Unknown <baggerone@gmail.com>
2018-09-01 10:46:09 -0400

    changed long form of confirm flag to "yes". Check for more than 0 args.

--- soda/cmd/drop.go
+++ soda/cmd/drop.go
@@ -17,8 +17,8 @@ var dropCmd = &cobra.Command{
 	Short: "Drops databases for you",
 	Run: func(cmd *cobra.Command, args []string) {
 		var err error
-		if len(args) > 1 {
-			err = fmt.Errorf("No arguments are allowed with the drop database command.")
+		if len(args) > 0 {
+			err = fmt.Errorf("No arguments allowed with the drop database command.")
 			fmt.Println(err)
 			os.Exit(1)
 		}
@@ -51,6 +51,6 @@ var dropCmd = &cobra.Command{
 
 func init() {
 	dropCmd.Flags().BoolVarP(&all, "all", "a", false, "Drops all of the databases in the database.yml")
-	dropCmd.Flags().BoolVarP(&confirmed, "confirmed", "y", false, "Runs without asking the user for confirmation")
+	dropCmd.Flags().BoolVarP(&confirmed, "yes", "y", false, "Runs without asking the user for confirmation")
 	RootCmd.AddCommand(dropCmd)
 }
