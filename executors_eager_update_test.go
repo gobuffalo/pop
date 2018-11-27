a5424c87f6a9881f7e51a5a773987cc7c14fd85e
Carl Lewis <nemesisesq@gmail.com>
2018-11-27 11:22:45 -0500

    added many to many test

--- executors_eager_update_test.go
+++ executors_eager_update_test.go
@@ -245,3 +245,51 @@ func Test_Eager_Update_Has_One(t *testing.T) {
 		
 	})
 }
+
+func Test_Eager_Update_Many_To_Many(t *testing.T) {
+	transaction(func(tx *Connection) {
+		r := require.New(t)
+		count2, _ := tx.Count(&User{})
+		println("Count of users in Eager: ", count2)
+		
+		user := User{
+			Name: nulls.NewString("Carl Lewis"),
+			Books: Books{
+				{Title: "Pop Book", Description: "Pop Book", Isbn: "PB1"},
+			},
+			FavoriteSong: Song{Title: "Hook - Blues Traveler"},
+			Houses: Addresses{
+				Address{HouseNumber: 86, Street: "Modelo"},
+			},
+		}
+		
+		
+		// Create User
+		err := tx.Eager().Create(&user)
+		
+		u := User{}
+		q := tx.Eager().Where("name = ?", "Carl Lewis")
+		err = q.First(&u)
+		
+		count := len(u.Houses)
+		
+		// Add two houses
+		u.Houses = append(u.Houses, Address{HouseNumber: 43, Street: "Bryden"})
+		u.Houses = append(u.Houses, Address{HouseNumber: 455, Street: "Broad"})
+		
+		// Remove the first house
+		
+		u.Houses = u.Houses[1:]
+		
+		tx.Eager().Update(&u)
+		
+		u2 := User{}
+		q2 := tx.Eager().Where("name = ?", "Carl Lewis")
+		err = q2.First(&u2)
+		
+		r.NoError(err)
+		r.Equal(count+1, len(u2.Houses))
+		
+		
+	})
+}
