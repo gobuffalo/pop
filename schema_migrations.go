a950d701dbbe1e9ad90f4714e69e83ccf184a4c7
Stanislas Michalak <stanislas.michalak@gmail.com>
2018-11-28 21:13:13 +0100

    Fix version field size in migrations table
    This will prevent big indexes for nothing.

--- schema_migrations.go
+++ schema_migrations.go
@@ -12,7 +12,13 @@ func newSchemaMigrations(name string) fizz.Table {
 	return fizz.Table{
 		Name: name,
 		Columns: []fizz.Column{
-			{Name: "version", ColType: "string"},
+			{
+				Name:    "version",
+				ColType: "string",
+				Options: map[string]interface{}{
+					"size": 14, // len(YYYYMMDDhhmmss)
+				},
+			},
 		},
 		Indexes: []fizz.Index{
 			{Name: fmt.Sprintf("%s_version_idx", name), Columns: []string{"version"}, Unique: true},
