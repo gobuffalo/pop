9d7cfcb410b3438d1f15d0da9941a0e01f5d0692
Stanislas Michalak <stanislas.michalak@gmail.com>
2019-03-18 15:07:06 +0100

    Remove legacy Anko format fixer
    It can still be called using soda fix command.

--- file_migrator.go
+++ file_migrator.go
@@ -7,12 +7,9 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
-	"strings"
 	"text/template"
 
 	"github.com/gobuffalo/fizz"
-	"github.com/gobuffalo/pop/fix"
-	"github.com/gobuffalo/pop/logging"
 	"github.com/pkg/errors"
 )
 
@@ -102,27 +99,13 @@ func migrationContent(mf Migration, c *Connection, r io.Reader) (string, error) 
 		return "", nil
 	}
 
-	content := string(b)
-
-	if mf.Type == "fizz" {
-		// test for && fix anko migrations
-		fixed, err := fix.Anko(content)
-		if err != nil {
-			return "", errors.Wrapf(err, "could not fizz the migration %s", mf.Path)
-		}
-		if strings.TrimSpace(fixed) != strings.TrimSpace(content) {
-			log(logging.Warn, "%s uses an old fizz syntax. please use\n%s", mf.Path, fixed)
-		}
-		content = fixed
-	}
-
-	t := template.Must(template.New("sql").Parse(content))
+	t := template.Must(template.New("sql").Parse(string(b)))
 	var bb bytes.Buffer
 	err = t.Execute(&bb, c.Dialect.Details())
 	if err != nil {
 		return "", errors.Wrapf(err, "could not execute migration template %s", mf.Path)
 	}
-	content = bb.String()
+	content := bb.String()
 
 	if mf.Type == "fizz" {
 		content, err = fizz.AString(content, c.Dialect.FizzTranslator())
