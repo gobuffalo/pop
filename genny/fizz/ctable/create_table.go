2a6624ab550a57a346b68810d6115dcef80cf3cd
Stanislas Michalak <stanislas.michalak@gmail.com>
2019-08-24 09:38:06 +0200

    Add support for columns in fizz create table

--- genny/fizz/ctable/create_table.go
+++ genny/fizz/ctable/create_table.go
@@ -1,6 +1,8 @@
 package ctable
 
 import (
+	"strings"
+
 	"github.com/gobuffalo/fizz"
 	"github.com/gobuffalo/genny"
 	"github.com/pkg/errors"
@@ -16,7 +18,51 @@ func New(opts *Options) (*genny.Generator, error) {
 	}
 
 	t := fizz.NewTable(opts.TableName, map[string]interface{}{})
+	for _, attr := range opts.Attrs {
+		o := fizz.Options{}
+		name := attr.Name.Underscore().String()
+		colType := fizzColType(attr.CommonType())
+		if name == "id" {
+			o["primary"] = true
+		}
+		if strings.HasPrefix(attr.GoType(), "nulls.") {
+			o["null"] = true
+		}
+		if err := t.Column(name, colType, o); err != nil {
+			return g, err
+		}
+	}
 	f := genny.NewFileS(opts.Name, t.String())
 	g.File(f)
 	return g, nil
+}
+
+func fizzColType(s string) string {
+	switch strings.ToLower(s) {
+	case "int":
+		return "integer"
+	case "time", "datetime":
+		return "timestamp"
+	case "uuid.uuid", "uuid":
+		return "uuid"
+	case "nulls.float32", "nulls.float64":
+		return "float"
+	case "slices.string", "slices.uuid", "[]string":
+		return "varchar[]"
+	case "slices.float", "[]float", "[]float32", "[]float64":
+		return "numeric[]"
+	case "slices.int":
+		return "int[]"
+	case "slices.map":
+		return "jsonb"
+	case "float32", "float64", "float":
+		return "decimal"
+	case "blob", "[]byte":
+		return "blob"
+	default:
+		if strings.HasPrefix(s, "nulls.") {
+			return fizzColType(strings.Replace(s, "nulls.", "", -1))
+		}
+		return strings.ToLower(s)
+	}
 }
--- genny/fizz/ctable/create_table_test.go
+++ genny/fizz/ctable/create_table_test.go
@@ -3,6 +3,7 @@ package ctable
 import (
 	"testing"
 
+	"github.com/gobuffalo/attrs"
 	"github.com/gobuffalo/genny/gentest"
 	"github.com/stretchr/testify/require"
 )
@@ -10,27 +11,57 @@ import (
 func Test_New(t *testing.T) {
 	r := require.New(t)
 
-	g, err := New(&Options{
-		TableName: "widgets",
-		Name:      "create_widgets.fizz",
-	})
+	ats, err := attrs.ParseArgs("id:uuid", "created_at:timestamp", "updated_at:timestamp", "name", "description:text", "age:int", "bar:nulls.String")
 	r.NoError(err)
 
-	run := gentest.NewRunner()
-	run.With(g)
-
-	r.NoError(run.Run())
-
-	res := run.Results()
-
-	r.Len(res.Commands, 0)
-	r.Len(res.Files, 1)
-
-	f := res.Files[0]
-	r.Equal("create_widgets.fizz", f.Name())
-	r.Equal(`create_table("widgets") {
+	cases := []struct {
+		Options *Options
+		Result  string
+	}{
+		{
+			&Options{
+				TableName: "widgets",
+				Name:      "create_widgets.fizz",
+			},
+			`create_table("widgets") {
 	t.Timestamps()
-}`, f.String())
+}`,
+		},
+		{
+			&Options{
+				TableName: "widgets",
+				Name:      "create_widgets.fizz",
+				Attrs:     ats,
+			},
+			`create_table("widgets") {
+	t.Column("id", "uuid", {primary: true})
+	t.Column("name", "string", {})
+	t.Column("description", "text", {})
+	t.Column("age", "integer", {})
+	t.Column("bar", "string", {null: true})
+	t.Timestamps()
+}`,
+		},
+	}
+
+	for _, c := range cases {
+		g, err := New(c.Options)
+		r.NoError(err)
+
+		run := gentest.NewRunner()
+		run.With(g)
+
+		r.NoError(run.Run())
+
+		res := run.Results()
+
+		r.Len(res.Commands, 0)
+		r.Len(res.Files, 1)
+
+		f := res.Files[0]
+		r.Equal("create_widgets.fizz", f.Name())
+		r.Equal(c.Result, f.String())
+	}
 }
 
 func Test_New_Fail(t *testing.T) {
