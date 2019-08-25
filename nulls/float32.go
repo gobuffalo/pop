5755dac850ca44eefcc5f6e2e5113d6fe02caf60
Stanislas Michalak <stanislas-m@users.noreply.github.com>
2019-08-25 12:46:28 +0200

    Fix broken redirection for nulls (#427)

--- nulls/float32.go
+++ nulls/float32.go
@@ -6,8 +6,12 @@ import (
 
 // Float32 adds an implementation for float32
 // that supports proper JSON encoding/decoding.
-type Float32 nulls.Float32
+//
+// Deprecated: use github.com/gobuffalo/nulls#Float32 instead.
+type Float32 = nulls.Float32
 
 // NewFloat32 returns a new, properly instantiated
 // Float32 object.
+//
+// Deprecated: use github.com/gobuffalo/nulls#NewFloat32 instead.
 var NewFloat32 = nulls.NewFloat32
--- nulls/uint32.go
+++ nulls/uint32.go
@@ -6,8 +6,12 @@ import (
 
 // UInt32 adds an implementation for int
 // that supports proper JSON encoding/decoding.
-type UInt32 nulls.UInt32
+//
+// Deprecated: use github.com/gobuffalo/nulls#UInt32 instead.
+type UInt32 = nulls.UInt32
 
 // NewUInt32 returns a new, properly instantiated
 // Int object.
+//
+// Deprecated: use github.com/gobuffalo/nulls#NewUInt32 instead.
 var NewUInt32 = nulls.NewUInt32
