535a7bd9b6950ccd3c0fc328155ff6e906318a6a
Jonathan Duck <jonathan@ec2software.com>
2019-08-27 13:23:39 -0700

    Fixed column order (#430)
    * Add test to demonstrate INSERT fails with a suffix on quoted column name #429
    
    * Ensure columns are sorted the same way each time.

--- columns/columns_test.go
+++ columns/columns_test.go
@@ -1,6 +1,7 @@
 package columns_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/gobuffalo/pop/columns"
@@ -59,4 +60,24 @@ func Test_Columns_Remove(t *testing.T) {
 		c.Remove("foo", "first_name")
 		r.Equal(len(c.Cols), 3)
 	}
+}
+
+type fooWithSuffix struct {
+	Amount      float64 `db:"amount"`
+	AmountUnits string  `db:"amount_units"`
+}
+type fooQuoter struct{}
+
+func (fooQuoter) Quote(key string) string {
+	return fmt.Sprintf("`%v`", key)
+}
+
+func Test_Columns_Sorted(t *testing.T) {
+	r := require.New(t)
+
+	c := columns.ForStruct(fooWithSuffix{}, "fooWithSuffix")
+	r.Equal(len(c.Cols), 2)
+	r.Equal(c.SymbolizedString(), ":amount, :amount_units")
+	r.Equal(c.String(), "amount, amount_units")
+	r.Equal(c.QuotedString(fooQuoter{}), "`amount`, `amount_units`")
 }
