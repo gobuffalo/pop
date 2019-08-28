9b1bf24032507de88ebaa5359fbe78875683f43c
Stanislas Michalak <stanislas.michalak@gmail.com>
2019-08-28 08:19:07 +0200

    Migrate fizz & SQL migrations generators to genny

--- soda/cmd/generate/sql_cmd.go
+++ soda/cmd/generate/sql_cmd.go
@@ -1,10 +1,13 @@
 package generate
 
 import (
+	"context"
 	"errors"
 
+	"github.com/gobuffalo/genny"
+	"github.com/gobuffalo/logger"
 	"github.com/gobuffalo/pop"
-	"github.com/gobuffalo/pop/internal/defaults"
+	"github.com/gobuffalo/pop/genny/fizz/cempty"
 	"github.com/spf13/cobra"
 )
 
@@ -13,11 +16,48 @@ var SQLCmd = &cobra.Command{
 	Use:   "sql [name]",
 	Short: "Generates Up/Down migrations for your database using SQL.",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		if len(args) == 0 {
-			return errors.New("you must supply a name for your migration")
+		name := ""
+		if len(args) > 0 {
+			name = args[0]
 		}
-		cflag := cmd.Flag("path")
-		migrationPath := defaults.String(cflag.Value.String(), "./migrations")
-		return pop.MigrationCreate(migrationPath, args[0], "sql", nil, nil)
+		run := genny.WetRunner(context.Background())
+
+		// Ensure the generator is as verbose as the old one.
+		lg := logger.New(logger.DebugLevel)
+		run.Logger = lg
+
+		p := cmd.Flag("path")
+		path := ""
+		if p != nil {
+			path = p.Value.String()
+		}
+		e := cmd.Flag("env")
+		type nameable interface {
+			Name() string
+		}
+		var translator nameable
+		db, err := pop.Connect(e.Value.String())
+		if err != nil {
+			return err
+		}
+		t := db.Dialect.FizzTranslator()
+		if tn, ok := t.(nameable); ok {
+			translator = tn
+		} else {
+			return errors.New("invalid fizz translator")
+		}
+
+		g, err := cempty.New(&cempty.Options{
+			TableName:  name,
+			Path:       path,
+			Type:       "sql",
+			Translator: translator,
+		})
+		if err != nil {
+			return err
+		}
+		run.With(g)
+
+		return run.Run()
 	},
 }
