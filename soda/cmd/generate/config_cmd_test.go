719c5ae65daa74d539434d2daba9f2f9c2cf1802
Stanislas Michalak <stanislas.michalak@gmail.com>
2019-12-03 22:23:40 +0100

    Remove GOPATH mentions in tests

--- soda/cmd/generate/config_cmd_test.go
+++ soda/cmd/generate/config_cmd_test.go
@@ -3,10 +3,8 @@ package generate
 import (
 	"io/ioutil"
 	"os"
-	"path/filepath"
 	"testing"
 
-	"github.com/gobuffalo/envy"
 	"github.com/stretchr/testify/require"
 )
 
@@ -15,10 +13,7 @@ func Test_ConfigCmd_NoArg(t *testing.T) {
 	c := ConfigCmd
 	c.SetArgs([]string{})
 
-	gp, err := envy.MustGet("GOPATH")
-	r.NoError(err)
-	cpath := filepath.Join(gp, "src", "github.com", "gobuffalo")
-	tdir, err := ioutil.TempDir(cpath, "testapp")
+	tdir, err := ioutil.TempDir("", "testapp")
 	r.NoError(err)
 	defer os.RemoveAll(tdir)
 
