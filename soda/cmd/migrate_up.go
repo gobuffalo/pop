2624c41ffa9432464f6cf62fb93d06b9e8acfa47
Patrik <zepatrik@users.noreply.github.com>
2020-04-20 14:39:08 +0200

    feat: add migrate UpTo(step) (#534)
    * feat: add migrate UpTo(step)
    
    * return number of applied migrations from migrator.UpTo
    
    * fix soda

--- soda/cmd/migrate_up.go
+++ soda/cmd/migrate_up.go
@@ -5,18 +5,22 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var migrationStepUp int
+
 var migrateUpCmd = &cobra.Command{
 	Use:   "up",
-	Short: "Apply all of the 'up' migrations.",
+	Short: "Apply one or more of the 'up' migrations.",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		mig, err := pop.NewFileMigrator(migrationPath, getConn())
 		if err != nil {
 			return err
 		}
-		return mig.Up()
+		_, err = mig.UpTo(migrationStepUp)
+		return err
 	},
 }
 
 func init() {
 	migrateCmd.AddCommand(migrateUpCmd)
+	migrateUpCmd.Flags().IntVarP(&migrationStepUp, "step", "s", 0, "Number of migrations to apply. Use 0 to apply all pending.")
 }
