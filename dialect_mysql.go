4b6dbd8c4bf46534965ab0ef1e77a73596144efd
zepatrik <zepatrik@users.noreply.github.com>
2021-07-14 17:50:25 +0200

    fix: mysql 5.7 supports no `DELETE FROM ... AS ...` but only `DELETE FROM ...`

--- dialect_mysql.go
+++ dialect_mysql.go
@@ -97,7 +97,13 @@ func (m *mysql) Destroy(s store, model *Model) error {
 }
 
 func (m *mysql) Delete(s store, model *Model, query Query) error {
-	return genericDelete(s, model, query)
+	sb := query.toSQLBuilder(model)
+
+	sql := fmt.Sprintf("DELETE FROM %s", m.Quote(model.TableName()))
+	sql = sb.buildWhereClauses(sql)
+
+	_, err := genericExec(s, sql, sb.Args()...)
+	return errors.Wrap(err, "mysql delete")
 }
 
 func (m *mysql) SelectOne(s store, model *Model, query Query) error {
