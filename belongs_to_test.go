e3e23555eedeeaa4edadddccf2e60b679f02e15c
Matthias <5011972+fasmat@users.noreply.github.com>
2026-04-18 13:46:31 +0200

    Fix assertion

--- belongs_to_test.go
+++ belongs_to_test.go
@@ -42,10 +42,10 @@ func Test_BelongsToThrough(t *testing.T) {
 	r := require.New(t)
 
 	q := PDB.BelongsToThrough(&User{ID: 1}, &Friend{})
-	qs := strings.TrimSpace(`
-	SELECT enemies.A FROM enemies AS enemies, good_friends AS good_friends
-	WHERE good_friends.user_id = ? AND enemies.id = good_friends.enemy_id
-	`)
+	qs := strings.Join([]string{
+		"SELECT enemies.A FROM enemies AS enemies, good_friends AS good_friends",
+		"WHERE good_friends.user_id = ? AND enemies.id = good_friends.enemy_id",
+	}, " ")
 
 	m := NewModel(new(Enemy), t.Context())
 	sql, _ := q.ToSQL(m)
